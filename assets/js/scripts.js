27d6bcb5495629cd15d5112fbfdb2f897aac18f4
MARY BETH CLARK <marybeth.clark@icloud.com>
2016-05-30 21:52:57 -0400

    initial commit

---
+++ assets/js/scripts.js
@@ -0,0 +1,39 @@
+
+// HMM...  swapStyleSheet is not defined, says the console.  If I had more time
+// tonight, I'd dig further into how to define it.  My brain is overflowing for now.
+function swapStyleSheet(sheet){
+  document.getElementById('pagestyle').SetAttribute('href',sheet)
+}
+console.log(hello);
+
+
+// function changeView () {
+//   var curView = document.getElementsByClass('list');
+//   if (changeView).hasClass("list"){
+//   ("#container ul").removeClass("grid").addClass("list");
+// }
+// var x = document.getElementsByClass('list');
+
+// if (x).hasClass("list"){
+// ("#container ul").removeClass("grid").addClass("list");
+// }
+
+// code below is from http://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_element_getattribute2
+// I though it would be a good starting point to translate it before attempting
+// a more complex function.
+// Hey JS, get ready to run a function by the oh-so-creative name of myFunction().
+ // function myFunction() {
+// I'm creating a variable named x and assigning it the value of the myBtn Id's
+ // attribute when clicked, which is to RUN the myFunction() function???
+  //  var x =
+  //  document.getElementById('demo').style.color ="red";
+ }
+// now look through the document for the demo Id.  When you find it, add text to
+// the HTML file equal to the value of the x variable, which is the onclick
+// attribute's value called myFunction().  Really? It will just insert the function name into
+ // the <p element??
+  //  document.getElementById('demo').innerHTML = x;
+
+
+ // not sure why this won't run.  Is it because the original code I was trying
+ // to duplicate had its js function in the HTML FILE?
